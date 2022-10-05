2fa618b5573e51f58688d06c5c0ae979f97aa3b8
Hans Ellis <bullhorn@gmail.com>
2022-10-04 19:15:47 -0700

    pass test for victory logic

---
+++ __tests__/race-system.test.js
@@ -0,0 +1,31 @@
+import RaceSystem from "./../src/js/race-system.js";
+import Character from "./../src/js/character.js";
+
+describe("Race system", () => {
+  test('Based on game stage will determine NPC enemy and battle specifics.', () => {
+    const charName = "Dave";
+    const charCar = "electric";
+    const stage = 1;
+    
+    const playerCharacter = new Character(charName,charCar);
+    const npcEnemy = new Character("Big Bubba","monster truck"); //need to add utility
+    const newRace = new RaceSystem(playerCharacter, npcEnemy, stage);
+    expect(newRace.playerOne).toEqual(playerCharacter);
+    expect(newRace.playerTwo).toEqual(npcEnemy);
+    expect(newRace.stage).toEqual(1);
+  });
+
+  test('it should take playerOne input and npc input to determine winner', () => {
+    const charName = "Dave";
+    const charCar = "electric";
+    const stage = 1;
+    
+    const playerCharacter = new Character(charName,charCar);
+    const npcEnemy = new Character("Big Bubba","monster truck"); //need to add utility
+    const newRace = new RaceSystem(playerCharacter, npcEnemy, stage);
+    
+    expect(newRace.runRace()).toEqual(true);
+    expect(newRace.runRace()).toEqual(false);
+    
+  })
+})
---
+++ src/js/race-system.js
@@ -0,0 +1,47 @@
+//should add utility runback function to edit object instance based on 
+//stage/level of Game.
+import { Character } from "./character.js";
+
+// Utility Logic
+
+function rollDice(carType) {// refactor code 
+  let finalScore = 0;
+
+  if (carType === "tuner") {
+    diceType = 0;
+  } else if  (carType === "electric")  {
+    diceType = 6;
+  } else if (carType === "muscle") {
+    diceType = 8;
+  } else if (carType === "bike") {
+    diceType = 12;
+  } else if  (carType === "monster truck") {
+    diceType = 4;
+  } else diceType = 3;
+
+  let dieRoll = Math.floor((Math.random() * 6) + 1);
+  console.log(dieRoll);
+  return dieRoll;
+}
+
+export default class RaceSystem {
+  constructor(playerCharacter, npcEnemy, stage) {
+    this.playerOne = playerCharacter;
+    this.playerTwo = npcEnemy;
+    this.stage = stage;
+    this.carDice = 0;
+  }
+
+  runRace() {
+    let p1 = this.playerOne;
+    let npc = this.playerTwo;
+    p1.carDice = rollDice();
+    npc.carDice = rollDice("monster truck");
+    console.log(p1.carDice);
+    console.log(npc.carDice);
+
+    if (p1.carDice > npc.carDice) {
+      return true;
+    } else return false;
+  }
+}
